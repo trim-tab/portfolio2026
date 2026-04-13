7609a65795ea2e7fee65d5d2bfedd2f7e05af91d
trim-tab <sjohnsonfilm@gmail.com>
2026-04-13 02:25:29 -0700

    added CommentBlock border to bb8 page

--- src/app/(app)/app/bb8/page.tsx
+++ src/app/(app)/app/bb8/page.tsx
@@ -1,11 +1,13 @@
 import Photo from "@/components/Photo";
-
+import ContentBlock from "@/components/ContentBlock";
 
 const page = () => {
   return (
 
     <>
+    <ContentBlock>
       <Photo />
+      </ContentBlock>
     </>
   );
 };
