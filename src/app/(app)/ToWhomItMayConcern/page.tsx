47f228be1f94f528a03f9313a0f83c448d4f3953
trim-tab <sjohnsonfilm@gmail.com>
2026-04-17 15:07:53 -0700

    edited to whom it may concern

--- src/app/(app)/ToWhomItMayConcern/page.tsx
+++ src/app/(app)/ToWhomItMayConcern/page.tsx
@@ -9,7 +9,27 @@ export default function page() {
       <div className="mt-5 grid grid-cols-3 grid-rows-[45px_1fr] gap-4">
         <div className="row-start-1 row-span-full col-start-1 col-span-full">
           <ContentBlock>
-            Test
+            
+To Whom It May Concern:
+<p className="pb-5">
+Are you familiar with this idea of Umwelt?  In ethology- the idea that organisms within the same ecosystem would have entirely different experiences.  I'm not certain how this concept came to into my vernacular, but I can't stop thinking about it.  The idea that one person's existential crisis might just be a mundane Tuesday afternoon for the other party.  
+
+</p>
+<p className="pb-5">
+I couldn't put my finger on what exactly was happening, but then I remembered a discussion I was having with an engineer that, quicker that I could realize, it became a disagreement, that, given room to breath, burst almost instantaneously into an argument.  It was a lesson that being right and having that be useful are two separate things.
+
+</p>
+<p className="pb-5">
+Our discussion was about units.  I have a penchant for flexible thinking.  The most common thought of questions to occur to me is most often "why not?" Say you took 1 and made it 1.3 it seemed to me entirely plausible that there could be times that that was useful. I proposed that there could be an instance where adjusting to fractional units could be possible. He dug in taking an aggressively defensive stance that seemed disproportionate to the topic at hand.  I couldn't understand why he was getting so upset.  Then I realized it.  I was attacking a cornerstone of his belief system, introducing chaos into his world ruled by nice neat integers.  
+</p>
+<p>
+Having grown up in chaos, upon chaotic chaos masquerading as order, there is a certain comfort I have with the chaotic that baffles those who walk through worlds of neat, tidy lines; orderly possessions, artifacts neatly tucked away behind stantions of convention.  When I want to do orderly things it's effort: keeping a clean work area I resort to knolling, straight lines, boundaries.  When I want to be creative I simply allow the elves to come in and muss up the place.  It makes one wonder if I could, perhaps, cure dyscalculia and dyslexia with order.  
+</p>
+<p className="pb-5">
+Having for the first time control over my environment it begs the question: 
+"Why not?"
+</p>
+
           </ContentBlock>
         </div>
       </div>
