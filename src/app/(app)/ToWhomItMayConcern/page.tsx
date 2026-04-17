99a41d21381db40a2ab9763fa27a86f38b656367
trim-tab <sjohnsonfilm@gmail.com>
2026-04-16 19:39:34 -0700

    twimc

---
+++ src/app/(app)/ToWhomItMayConcern/page.tsx
@@ -0,0 +1,18 @@
+
+import ContentBlock from "@/components/ContentBlock";
+
+import Post from "@/components/Post"; 
+
+export default function page() {
+  return (
+    <>
+      <div className="mt-5 grid grid-cols-3 grid-rows-[45px_1fr] gap-4">
+        <div className="row-start-1 row-span-full col-start-1 col-span-full">
+          <ContentBlock>
+            Test
+          </ContentBlock>
+        </div>
+      </div>
+    </>  
+   );
+}
